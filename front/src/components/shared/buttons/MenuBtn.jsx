395bb7059eec7e87d7a9c224dcbb0e1468c91ad4
Bartosz Bartosiewicz <im.bartek@o2.pl>
2021-02-17 21:42:34 +0100

    created global store and logic for showing/hiding menu

--- front/src/components/shared/buttons/MenuBtn.jsx
+++ front/src/components/shared/buttons/MenuBtn.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import './MenuBtn.css';
+import { toggleMenu } from '../../../actions/toogleMenu.actions';
 
-const MenuBtn = () => {
+const MenuBtn = ({ toggleMenu }) => {
+
+    const [openMenu, setOpenMenu] = useState(false)
 
     const handleClick = (e) => {
         const menu = document.querySelector('.menu_btn');
         menu.classList.toggle('open');
+        setOpenMenu(!openMenu)
+        toggleMenu(!openMenu)
     }
     return (
         <div className="menu_btn" onClick={handleClick}>
@@ -17,4 +23,8 @@ const MenuBtn = () => {
     );
 }
 
-export default MenuBtn;
+const mapDispatchToProps = dispatch => ({
+    toggleMenu: (isOpen) => dispatch(toggleMenu(isOpen))
+})
+
+export default connect(null, mapDispatchToProps)(MenuBtn);
---
+++ front/src/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import toggleMenu from './toggleMenuReducer';
+
+export default combineReducers({
+    toggleMenu
+})

2abac9ffd7dea00a2a11e49e846121fec7cc24c3
Bartosz Bartosiewicz <bartoszbartosiewicz@MacBook-Air-Bartosz.local>
2023-01-23 16:39:47 +0100

    created route to get product id

--- server/controllers/products/productsController.js
+++ server/controllers/products/productsController.js
@@ -16,6 +16,20 @@ router.get('/', function(req, res, next){
 
 })
 
+router.get('/:id', function(req, res, next){
+    const id = req.query.id
+    const usersQuerry = `SELECT * FROM products WHERE id = ${id}`;
+
+    client.query(usersQuerry, function(err, result){
+        if(err){
+            throw err;
+        }
+        console.log(result)
+        res.json(result)
+    })
+
+})
+
 
 
 module.exports = router;

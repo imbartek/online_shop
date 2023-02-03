54d6add1f8d5d702bd6e7709546aa6f72e381fff
Bartosz Bartosiewicz <bartoszbartosiewicz@MacBook-Air-Bartosz.local>
2023-02-03 20:05:25 +0100

    created CRUD for products

--- server/controllers/products/productsController.js
+++ server/controllers/products/productsController.js
@@ -30,6 +30,61 @@ router.get('/:id', function(req, res, next){
 
 })
 
+router.post('/create', function(req, res, next){
+    const name = req.body.name;
+    const desc = req.body.desc;
+    const price = req.body.price;
+    const image = req.body.image;
+    const id_category = req.body.id_category;
 
+    const insertQuery = `INSERT INTO products (name, description, price, image, id_category) VALUES ('${name}', '${desc}', ${price}, ${image}, ${id_category} ) returning id`;
+
+    client.query(insertQuery, [], function(req, res, next){
+        if(err){
+            throw err;
+        }
+        console.log(result)
+        res.json('Produkt dodany pomyślnie')
+    })
+})
+
+router.post('/update', function(req, res, next){
+    const id = req.body.id;
+    const name = req.body.name;
+    const desc = req.body.desc;
+    const price = req.body.price;
+    const image = req.body.image;
+    const id_category = req.body.id_category;
+
+    const nameQ = name ? ` name = '${name}'` : '';
+    const descQ = desc ? ` desc = '${desc}'` : '';
+    const priceQ = price ? ` price = ${price}` : '';
+    const imageQ = image ? ` image = ${image}` : '';
+    const idCatQ = id_category ? ` id_category = ${id_category}` : '';
+
+    const updateQuery = `UPDATE products SET ${nameQ}, ${descQ}, ${priceQ}. ${imageQ}, ${idCatQ} WHERE id = ${id}`;
+
+    client.query(updateQuery, [], function(err, result){
+        if(err){
+            throw err;
+        }
+        console.log(result)
+        res.json('Produkt został edytowany');
+    })
+})
+
+router.post('delete', function(req, res, next){
+    const id = req.body.id;
+
+    const deleteQuery = `DELETE FROM products WHERE id = ${id}`;
+
+    client.query(deleteQuery, [], function(err, result){
+        if(err){
+            throw err;
+        }
+        console.log(result)
+        res.json('Produkt został usunięty');
+    })
+})
 
 module.exports = router;

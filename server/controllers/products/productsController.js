b44b400804f5daf8e1ff7c484be9b5058638c0af
Bartosz Bartosiewicz <bartoszbartosiewicz@MacBook-Pro-Bartosz.local>
2023-04-15 09:35:14 +0200

    updated: query => params

--- server/controllers/products/productsController.js
+++ server/controllers/products/productsController.js
@@ -17,7 +17,8 @@ router.get('/', function(req, res, next){
 })
 
 router.get('/:id', function(req, res, next){
-    const id = req.query.id
+    console.log(req.params.id)
+    const id = req.params.id
     const usersQuerry = `SELECT products.*, sizes.value, sizes.quantity FROM products LEFT JOIN sizes ON products.id = sizes.id_product WHERE products.id = ${id}`;
 
     client.query(usersQuerry, function(err, result){
